dbc05f55fd1084a613cfd9deec4c2a39b45e5a2a
Sajid Alam <sajidalam786@gmail.com>
2025-05-17 21:08:59 +0100

    Create certificates.js

---
+++ src/data/certificates.js
@@ -0,0 +1,38 @@
+export const certificates = [
+  {
+    id: "1",
+    Title: "React Developer Certification",
+    Img: "/certificate-images/react-cert.jpg",
+    Issuer: "Frontend Masters"
+  },
+  {
+    id: "2",
+    Title: "JavaScript Algorithms and Data Structures",
+    Img: "/certificate-images/js-cert.jpg",
+    Issuer: "freeCodeCamp"
+  },
+  {
+    id: "3",
+    Title: "Responsive Web Design",
+    Img: "/certificate-images/responsive-cert.jpg",
+    Issuer: "freeCodeCamp"
+  },
+  {
+    id: "4",
+    Title: "UX Fundamentals",
+    Img: "/certificate-images/ux-cert.jpg",
+    Issuer: "Google"
+  },
+  {
+    id: "5",
+    Title: "Git & GitHub Professional",
+    Img: "/certificate-images/git-cert.jpg",
+    Issuer: "Udemy"
+  },
+  {
+    id: "6",
+    Title: "TailwindCSS Mastery",
+    Img: "/certificate-images/tailwind-cert.jpg",
+    Issuer: "Scrimba"
+  }
+];
